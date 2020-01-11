5c903a9bb6107865d9ceda2329be9d1465dbabe3
Dominic Wüthrich <dw@cabtronix.ch>
2020-01-11 10:36:52 +0100

    update crime.sh
    Signed-off-by: Dominic Wüthrich <dw@cabtronix.ch>

--- crime.sh
+++ crime.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#
+# crime.sh
+# crime menu script
+#
+# 11.01.2020 / Dominic Wuethrich, Simon Tschudi, (Beat Maier)
+#
+
+# Task 8.1: hier kommen die kommentare, dann die skripts
+# hier Skripts, ohne raute natürlich
+
+# usw
