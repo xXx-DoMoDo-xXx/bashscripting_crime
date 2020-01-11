8105c8d8f4e89c1cdbdc3a0c2c57365439c3f966
DoMoDo <dw@cabtronix.ch>
2020-01-11 11:10:12 +0100

    update crime.sh
    Signed-off-by: DoMoDo <dw@cabtronix.ch>

--- crime.sh
+++ crime.sh
@@ -6,7 +6,18 @@
 # 11.01.2020 / Dominic Wuethrich, Simon Tschudi, (Beat Maier)
 #
 
-# Task 8.1: hier kommen die kommentare, dann die skripts
-# hier Skripts, ohne raute natürlich
+# Task 8.1: Funktion: Dateistruktur Analyse
+# Anzahl Zeilen zaehlen
+cat crime.csv  | wc -l
 
-# usw
+# Anzahl Spalten zaehlen
+awk -F',' 'NR==1{print NF}' crime.csv
+
+# Nur Spaltenbezeichnung anzeigen
+awk 'NR==1 {print; exit}' crime.csv
+
+# Funktion: Daten Analyse
+
+# Funktion: Häufigstes Verbrechen pro Stadt
+
+# Funktion: Beste Stadt in Australien
